976ed9961fc04ef69c32548d42b37fea5f929ce4
luismartinezs <martinez.s.luis@gmail.com>
2021-04-25 23:11:52 +0200

    transition group

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Modal</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/list">List</router-link>
     </div>
     <transition name="slide-fade" mode="out-in">
       <router-view />
@@ -18,33 +18,22 @@
   text-align: center;
   color: #2c3e50;
 }
-
 button {
-  width: 5em;
-  height: 2em;
+  height: 2.2em;
   margin: 0.5em;
-  border-radius: 5px;
+  border-radius: 0.3em;
   background: linear-gradient(to right, #16c0b0, #84cf6a);
-  font-size: 1em;
   color: white;
-  border: none;
-  outline: none;
-  cursor: pointer;
 }
-
-#nav {
-  padding: 30px;
+input {
+  display: inline-block;
+  box-sizing: border-box;
+  width: 30%;
+  height: 1.8em;
+  padding: 0.5em;
+  margin: 0.2em;
+  font: 0.8em 'Avenir', Helvetica, sans-serif;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-
 .modal {
   width: 250px;
   height: 200px;
@@ -54,30 +43,59 @@ button {
   border-radius: 2%;
   background-color: #e0e0e0;
 }
-
-/* Transitions */
-.fade-enter,
-.fade-leave-to {
+#nav {
+  padding: 30px;
+}
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+p {
+  font-size: 0.8em;
+}
+::placeholder {
+  color: #2c3e50;
+}
+ul {
+  list-style-type: none;
+}
+/*** TRANSITIONS ***/
+.fade-enter {
   opacity: 0;
 }
-
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.5s ease-out;
 }
-
-.slide-fade-enter {
+.fade-leave-to {
   opacity: 0;
-  transform: translateX(10px);
 }
 
+.slide-fade-enter {
+  transform: translateX(10px);
+  opacity: 0;
+}
 .slide-fade-enter-active,
 .slide-fade-leave-active {
   transition: all 0.2s ease;
 }
-
 .slide-fade-leave-to {
-  opacity: 0;
   transform: translateX(-10px);
+  opacity: 0;
+}
+
+.slide-up-enter {
+  transform: translateY(10px);
+  opacity: 0;
+}
+.slide-up-enter-active {
+  transition: all 0.2s ease;
+}
+
+.slide-up-move {
+  transition: transform 0.8s ease-in;
 }
 </style>
